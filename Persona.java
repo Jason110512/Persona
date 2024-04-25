7117716f725f2ca510cca3413360ede3945aedca
Jason Rojas <jasonrojasyy@gmail.com>
2024-04-25 12:28:46 -0600

    vesion 1

---
+++ Persona.java
@@ -0,0 +1,15 @@
+ class Persona {
+    String nombre,genero;
+    int edad; 
+    double altura;
+    public void mostrarNombre(){
+        System.out.println("mi nombre es "+this.nombre);
+    }
+    public void mostrarEdad(){
+        System.out.println("tengo: "+ this.edad +" años");
+    }
+    public void mostrarAltura(){
+        System.out.println("mi altura "+this.altura);
+    }
+    
+}
